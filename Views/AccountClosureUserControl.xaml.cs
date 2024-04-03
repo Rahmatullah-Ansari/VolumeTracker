227deac9f31ae7df53ed371bc8808a5cc2d655a3
Veerendra <veerendrayadav@globussoft.in>
2024-04-03 11:03:42 +0530

    Updated code

--- Views/AccountClosureUserControl.xaml.cs
+++ Views/AccountClosureUserControl.xaml.cs
@@ -17,8 +17,11 @@ namespace BankingApp.Views
         {
             if (!(string.IsNullOrEmpty(firstname.Text) || string.IsNullOrEmpty(lastname.Text) || string.IsNullOrEmpty(middlename.Text) ||
                 string.IsNullOrEmpty(fathersname.Text) || string.IsNullOrEmpty(mobilenumber.Text) || string.IsNullOrEmpty(aadharcardnummber.Text) ||
-                string.IsNullOrEmpty(permanentaddress.Text) || string.IsNullOrEmpty(mothersname.Text)))
-                MessageBox.Show("Account Created Successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                string.IsNullOrEmpty(permanentaddress.Text) || string.IsNullOrEmpty(mothersname.Text) || string.IsNullOrEmpty(phonenumber.Text)))
+            {
+                MessageBox.Show("Account Closed Successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                firstname.Text = lastname.Text = middlename.Text = fathersname.Text = mobilenumber.Text = aadharcardnummber.Text = phonenumber.Text = permanentaddress.Text = mothersname.Text = "";
+            }
             else
                 MessageBox.Show("All fields are mandatory", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
